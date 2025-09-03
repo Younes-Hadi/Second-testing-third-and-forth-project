3b4c60424a47d46bd1993ea62fed378bab128ea2
Younes <youneshadi07@gmail.com>
2025-09-03 12:43:28 +0330

    finish, waiting for merge

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import "./App.css";
 
 function App(){
-    const user={
-        FirstName:"Younes",
-        LastName:"Hadi",
-        Age:18,
-        City:"Tehran",
-        university:null,
-        school:"Mandegar Alborz"
-    };
+    const user=[
+        {FirstName:"Younes"},
+        {LastName:"Hadi"},
+        {Age:18},
+        {City:"Tehran"},
+        {university:null},
+       {school:"Mandegar Alborz"}
+    ];
     return(
         <div className="mainContainer">
             <h1>Hello guys!</h1>
             <br/>
-            {user.university? `Your university is ${user.university}` : `Your school is ${user.school}`}
+            {user?.university? `Your university is ${user[4].university}` : `Your school is ${user[5].school}`}
+            {user.map(data =>
+            <h4>{Object.values(data)?.[0]}</h4>
+            )}
         </div>
     )
 }
